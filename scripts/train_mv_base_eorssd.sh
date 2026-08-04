18a81970a27c38ec14acdf3b47a69f3f57467c0a
Rayon <1753368089@qq.com>
2026-08-04 23:16:54 +0800

    change path to orsi sod

---
+++ scripts/train_mv_base_eorssd.sh
@@ -0,0 +1,20 @@
+# scripts/train_mv_base_eorssd.sh
+#!/usr/bin/env bash
+cd /home/MLab
+
+python train.py \
+    --network models.networks.mambavision_small_sod \
+    --run-dir runs/mambavision_small_sod_cosine_duts \
+    --train-images datasets/DUTS/DUTS-TR/DUTS-TR-Image \
+    --train-masks datasets/DUTS/DUTS-TR/DUTS-TR-Mask \
+    --image-size 352 \
+    --epochs 30 \
+    --batch-size 8 \
+    --num-workers 8 \
+    --lr 1e-4 \
+    --min-lr 1e-6 \
+    --weight-decay 1e-4 \
+    --aux-weight 0.4 \
+    --seed 42 \
+    --save-every 5 \
+    --log-interval 100

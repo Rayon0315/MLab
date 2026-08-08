6f7028710c3409aa80201b800e83a0b5e945aa6b
Rayon <1753368089@qq.com>
2026-08-08 20:49:46 +0800

    adjust stage 4

---
+++ scripts/test_mambavision_small_spatial_progressive_sod_eorssd.sh
@@ -0,0 +1,20 @@
+# scripts/test_mambavision_small_spatial_progressive_sod_eorssd.sh
+#!/usr/bin/env bash
+
+cd /home/MLab
+
+RUN_DIR="runs/mambavision_small_spatial_progressive_sod_eorssd"
+
+python test.py \
+  --network models.networks.mambavision_small_spatial_progressive_sod \
+  --checkpoint "$RUN_DIR/checkpoints/final.pth" \
+  --test-images datasets/EORSSD/test-images \
+  --test-masks datasets/EORSSD/test-labels \
+  --dataset-name EORSSD \
+  --image-size 352 \
+  --batch-size 8 \
+  --num-workers 8 \
+  --device cuda \
+  --amp \
+  --output-dir "$RUN_DIR/test/EORSSD" \
+  --log-interval 100
